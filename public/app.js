dbd68209049163862e1278b9fcabb7b46c09feb3
Dumebi <chiwuzohdumebi@gmail.com>
2021-04-27 01:56:28 +0100

    page 3 update

--- public/app.js
+++ public/app.js
@@ -95,60 +95,63 @@ app.client.request = function (
 
 // Bind the forms
 app.bindForms = function () {
-  document.querySelector("form").addEventListener("submit", function (e) {
-    // Stop it from submitting
-    e.preventDefault();
-    var formId = this.id;
-    var path = this.action;
-    var method = this.method.toUpperCase();
+  if (document.querySelector("form")) {
+    document.querySelector("form").addEventListener("submit", function (e) {
+      // Stop it from submitting
+      e.preventDefault();
+      var formId = this.id;
+      var path = this.action;
+      var method = this.method.toUpperCase();
 
-    // Hide the error message (if it's currently shown due to a previous error)
-    document.querySelector("#" + formId + " .formError").style.display = "none";
+      // Hide the error message (if it's currently shown due to a previous error)
+      document.querySelector("#" + formId + " .formError").style.display =
+        "none";
 
-    // Turn the inputs into a payload
-    var payload = {};
-    var elements = this.elements;
-    for (var i = 0; i < elements.length; i++) {
-      if (elements[i].type !== "submit") {
-        var valueOfElement =
-          elements[i].type === "checkbox"
-            ? elements[i].checked
-            : elements[i].value;
-        payload[elements[i].name] = valueOfElement;
-      }
-    }
-
-    // Call the API
-    app.client.request(
-      undefined,
-      path,
-      method,
-      undefined,
-      payload,
-      function (statusCode, responsePayload) {
-        // Display an error on the form if needed
-        if (statusCode !== 200) {
-          // Try to get the error from the api, or set a default error message
-          var error =
-            typeof responsePayload.Error === "string"
-              ? responsePayload.Error
-              : "An error has occured, please try again";
-
-          // Set the formError field with the error text
-          document.querySelector(
-            "#" + formId + " .formError"
-          ).innerHTML = error;
-
-          // Show (unhide) the for error field on the form
-          document.querySelector("#" + formId + " .formError").style.display =
-            "block";
-        } else {
-          // If successful, send to form response processor
-          app.formResponseProcessor(formId, payload, responsePayload);
+      // Turn the inputs into a payload
+      var payload = {};
+      var elements = this.elements;
+      for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type !== "submit") {
+          var valueOfElement =
+            elements[i].type === "checkbox"
+              ? elements[i].checked
+              : elements[i].value;
+          payload[elements[i].name] = valueOfElement;
         }
       }
-    );
-  });
+
+      // Call the API
+      app.client.request(
+        undefined,
+        path,
+        method,
+        undefined,
+        payload,
+        function (statusCode, responsePayload) {
+          // Display an error on the form if needed
+          if (statusCode !== 200) {
+            // Try to get the error from the api, or set a default error message
+            var error =
+              typeof responsePayload.Error === "string"
+                ? responsePayload.Error
+                : "An error has occured, please try again";
+
+            // Set the formError field with the error text
+            document.querySelector(
+              "#" + formId + " .formError"
+            ).innerHTML = error;
+
+            // Show (unhide) the for error field on the form
+            document.querySelector("#" + formId + " .formError").style.display =
+              "block";
+          } else {
+            // If successful, send to form response processor
+            app.formResponseProcessor(formId, payload, responsePayload);
+          }
+        }
+      );
+    });
+  }
 };
 
 // Form response processor
