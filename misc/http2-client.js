6a83897e12579770cb07913431d5b14a5936036a
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-08-17 18:28:22 +0100

    http2

---
+++ misc/http2-client.js
@@ -0,0 +1,31 @@
+/*
+ * Example HTTP2 client
+ * Connects to port 6000 and logs the response
+ *
+ */
+
+// Dependencies
+var http2 = require("http2");
+
+// Create client
+var client = http2.connect("http://localhost:6000");
+
+// Create a request
+var req = client.request({
+  path: "/",
+});
+
+// When message is received, add the pieces of it together until you reach the end
+var str = "";
+req.on("data", function (chunk) {
+  console.log({ chunk: chunk.toString() });
+  str += chunk;
+});
+
+// When a message ends, log it out
+req.on("end", function () {
+  console.log({ end: str });
+});
+
+// End the request
+req.end();
